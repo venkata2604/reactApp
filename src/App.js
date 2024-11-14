91ec409eb2f5af00d48284b92af63e9f615173ef
Venkata Sai Aavula <venkatasaiaavula@gmail.com>
2024-11-13 20:03:33 -0600

    Intializing a new ReactApp

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <h2>My first React App</h2>
+      <p>Welcome to React class</p>
+    </div>
+  );
+}
+
+export default App;
+// JSX JavaScript XML. It allows us to write HTML inside a javascript file.
